2ed9631ea161bb182b485243b6215a3252693ec0
Ariton <aritonarm@gmail.com>
2023-01-23 00:02:44 +0100

    Sidebar fixes, PGP key middleware - implementation

--- routes/buyer.js
+++ routes/buyer.js
@@ -6,17 +6,18 @@ const jwt = require("jsonwebtoken");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const require2FA = require("../middleware/require2FA");
 const isBuyer = require("../middleware/isBuyer");
+const hasPGPKey = require("../middleware/hasPGPKey");
 
 // Import the database models
 const db = require("../models");
 const User = db.User;
 const Role = db.Role;
 
-router.get("/apply-for-vendor", isLoggedIn, require2FA, isBuyer, (req, res) => {
+router.get("/apply-for-vendor", isLoggedIn, require2FA, isBuyer, hasPGPKey, (req, res) => {
     res.render("buyer/apply-for-vendor");
 });
 
-router.post("apply-for-vendor", isLoggedIn, require2FA, isBuyer, (req, res) => {
+router.post("apply-for-vendor", isLoggedIn, require2FA, isBuyer, hasPGPKey, (req, res) => {
 
 })
 
